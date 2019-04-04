040bc2f6ba0c630d748e1d737670b85397a3b2f1
cabelo <cabelo@opensuse.org>
2019-04-04 14:16:59 -0300

    Code change for compatibility with OpenVINO version version 2019.R1

--- src/base_detection.hpp
+++ src/base_detection.hpp
@@ -16,11 +16,7 @@
 
 #include <inference_engine.hpp>
 
-#define INTEL_LAST_VER 5
-
-#if (INTEL_CVSDK_VER >= INTEL_LAST_VER)
 #include <samples/ocv_common.hpp>
-#endif
 
 #include <samples/common.hpp>
 #include <samples/slog.hpp>
@@ -30,6 +26,7 @@
 #include <boost/log/expressions.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
+#include <opencv2/opencv.hpp>
 
 typedef struct {
             std::vector<cv::Mat*> batchOfInputFrames;
