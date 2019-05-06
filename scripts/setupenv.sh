a3e02afcb9de7028111c9d70af2e3faff15ba7de
Hernán Gonzalez <hernan.gonzalez@incluit.com>
2019-05-06 16:58:32 -0300

    setupenv: Update new release models' path
    Signed-off-by: Hernán Gonzalez <hernan.gonzalez@incluit.com>

--- scripts/setupenv.sh
+++ scripts/setupenv.sh
@@ -17,8 +17,9 @@ else
 fi
 fi
 
-modelDir=$InferenceEngine_DIR/../../intel_models
 
+if [ $INTEL_CVSDK_VER -ne "6" ]; then
+modelDir=$InferenceEngine_DIR/../../intel_models
 # Vehicle and License Plates Detection Model
 modName=vehicle-license-plate-detection-barrier-0106
 export mVLP16=$modelDir/$modName/FP16/$modName.xml
@@ -49,6 +50,40 @@ export person132=$modelDir/$modName/FP32/$modName.xml
 modName=pedestrian-detection-adas-0002
 export person216=$modelDir/$modName/FP16/$modName.xml
 export person232=$modelDir/$modName/FP32/$modName.xml
+
+else
+modelDir=$InferenceEngine_DIR/../../tools/model_downloader
+# Vehicle and License Plates Detection Model
+modName=vehicle-license-plate-detection-barrier-0106
+export mVLP16=$modelDir/Security/object_detection/barrier/0106/dldt/$modName\-fp16.xml
+export mVLP32=$modelDir/Security/object_detection/barrier/0106/dldt/$modName.xml
+
+# Vehicle-only Detection Model used with the batch size exercise
+modName=vehicle-detection-adas-0002
+export mVDR16=$modelDir/Transportation/object_detection/vehicle/mobilenet-reduced-ssd/dldt/$modName\-fp16.xml
+export mVDR32=$modelDir/Transportation/object_detection/vehicle/mobilenet-reduced-ssd/dldt/$modName.xml
+
+# Vehicle Attributes Detection Model
+modName=vehicle-attributes-recognition-barrier-0039
+export mVA16=$modelDir/Security/object_attributes/vehicle/resnet10_update_1/dldt/$modName\-fp16.xml
+export mVA32=$modelDir/Security/object_attributes/vehicle/resnet10_update_1/dldt/$modName.xml
+
+modName=person-vehicle-bike-detection-crossroad-0078
+export vehicle216=$modelDir/Security/object_detection/crossroad/0078/dldt/$modName\-fp16.xml
+export vehicle232=$modelDir/Security/object_detection/crossroad/0078/dldt/$modName.xml
+
+modName=pedestrian-and-vehicle-detector-adas-0001
+export pv16=$modelDir/Transportation/object_detection/pedestrian-and-vehicle/mobilenet-reduced-ssd/dldt/$modName\-fp16.xml
+export pv32=$modelDir/Transportation/object_detection/pedestrian-and-vehicle/mobilenet-reduced-ssd/dldt/$modName.xml
+
+modName=person-detection-retail-0013
+export person116=$modelDir/Retail/object_detection/pedestrian/hypernet-rfcn/0026/dldt/$modName\-fp16.xml
+export person132=$modelDir/Retail/object_detection/pedestrian/hypernet-rfcn/0026/dldt/$modName.xml
+
+modName=pedestrian-detection-adas-0002
+export person216=$modelDir/Transportation/object_detection/pedestrian/mobilenet-reduced-ssd/dldt/$modName\-fp16.xml
+export person232=$modelDir/Transportation/object_detection/pedestrian/mobilenet-reduced-ssd/dldt/$modName.xml
+fi
 
 modName=frozen_yolo_v3
 export yolo16=../data/$modName.xml
