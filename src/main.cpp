abddb5395f2833ab63d53cd40629292b8d20441e
Flavio Priotti <flaviojpriotti@gmail.com>
2020-05-19 21:07:12 -0300

    Update for static code review

--- src/main.cpp
+++ src/main.cpp
@@ -86,7 +86,7 @@ std::string return_current_time_and_date()
     auto now = std::chrono::system_clock::now();
     auto in_time_t = std::chrono::system_clock::to_time_t(now);
     std::stringstream ss;
-    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d_%X"); // Format
+    ss << std::put_time(std::gmtime(&in_time_t), "%Y-%m-%d_%X"); // Format
     return ss.str();                                                // Return the datetime
 }
 
